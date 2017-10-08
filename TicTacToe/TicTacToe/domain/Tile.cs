60d86d28c2cd8b9120c3f2d71c62013d5bd65903
Cedric <Cedric>
2017-10-08 17:12:52 +0200

    added and implemented class Tile

---
+++ TicTacToe/TicTacToe/domain/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.domain
+{
+    class Tile
+    {
+        private Boolean _isFree;
+        private int _content;
+
+        public Tile(int content = 0)
+        {
+            _isFree = true;
+        }
+
+        public Boolean Free
+        {
+            get { return _content == 0; }
+            set { _isFree = value; }
+        }
+
+        public int Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                _isFree = true;
+            }
+        }
+    }
+}

cd773eabdb4fe45ee815d153e72ca7b08790dd94
Cedric <Cedric>
2017-10-19 11:28:28 +0200

    changed game loop

--- TicTacToe/TicTacToe/domain/Game.cs
+++ TicTacToe/TicTacToe/domain/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,43 @@ namespace TicTacToe.domain
         private GameBoard _gameBoard;
         private static readonly string _x = "X";
         private static readonly string _o = "O";
+        private int _turn;
 
         public Game()
         {
             _humanPlayer = new HumanPlayer("Cedric");
             _aiPlayer = new AIPlayer();
             _gameBoard = new GameBoard();
+        }
+
+        public async void StartGame()
+        {
+            Random rnd = new Random();
+            while (true)
+            { 
+                Turn = 0;
+                while (Turn == 0)
+                {
+                    await WaitForButtonClickAsync();
+                }
+                while (Turn == 1)
+                {
+                    var number = rnd.Next(9) + 1;
+                    GameBoard.Board[number].Content = O;
+                    Turn = 0;
+                }
+            }
+        }
+
+        Task WaitForButtonClickAsync()
+        {
+            return Task.Delay(1);
+        }
+
+        public int Turn
+        {
+            get { return _turn; }
+            set { _turn = value; }
         }
 
         public Player HumanPlayer
